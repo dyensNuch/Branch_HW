2983ed79e8dbff47a0911ce9677becfe6114f93d
dyensNuch <dyens@hotmail.com>
2016-06-11 21:14:32 +0700

    "Version1.0" from master git

--- app/src/main/java/com/example/nuch_nb/gitbranch_hw/MainActivity.java
+++ app/src/main/java/com/example/nuch_nb/gitbranch_hw/MainActivity.java
@@ -9,5 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String mVersion = "Version1.0" //from master git;
     }
 }

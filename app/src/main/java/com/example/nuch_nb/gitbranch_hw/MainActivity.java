72cdbf6c804b7b76ea9599a37c192e3f17f89988
dyensNuch <dyens@hotmail.com>
2016-06-11 17:07:06 +0700

    Init Commit

---
+++ app/src/main/java/com/example/nuch_nb/gitbranch_hw/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.nuch_nb.gitbranch_hw;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
